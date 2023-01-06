f9b54ebc032b692e78218cf227f0e3a1d19f4083
a.feyznia <azam.feyznia@gmail.com>
2023-01-06 13:15:55 +0330

    adding seeder for users table

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Author One',
+            'email' => 'author1@gmail.com',
+        ]);
+    }
+}
